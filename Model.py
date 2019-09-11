d16e489e67f8917c9c59048eab7ed7897a4f98fe
gandharv <gandharv.suri@iiitb.org>
2019-09-11 16:24:50 +0530

    Starting Missing Data Handling

--- Model.py
+++ Model.py
@@ -9,4 +9,16 @@ from sklearn.utils import shuffle
 #Load data
 data = pd.read_csv("Pima_Indian_diabetes.csv")
 
-print(data.BMI.isnull().sum())  
+
+#CHECK FOR MISSING DATA
+# 26 print(data.Pregnancies.isnull().sum()) 
+#16 print(data.Glucose.isnull().sum())
+#0 print(data.BloodPressure.isnull().sum()) 
+# 22 print(data.SkinThickness.isnull().sum()) 
+#0 print(data.Insulin.isnull().sum()) 
+# 11 print(data.BMI.isnull().sum()) 
+# 0 print(data.DiabetesPedigreeFunction.isnull().sum())
+# 19 print(data.Age.isnull().sum())
+# 0 print(data.Outcome.isnull().sum())
+
+print(data.BMI.value_counts())
